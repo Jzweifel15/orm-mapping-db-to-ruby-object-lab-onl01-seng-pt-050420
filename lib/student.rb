f1c8e083188832bec6e0bd1c411eef55227d5baa
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-02 12:30:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,9 +54,10 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
-    
+  def self.first_X_students_in_grade_10()
   end
+  
+  def self.
   
   def save
     sql = <<-SQL

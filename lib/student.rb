6dc950f5d7021c4eedcc7f6339833f509eb09e9b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-02 12:39:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,15 @@ class Student
     end
   end
   
-  def self.
+  def self.all_students_in_grade_X(grade_num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = "?";
+    SQL
+    DB[:conn].execute(sql, grade_num).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   def self.all_students_in_grade_X
   end

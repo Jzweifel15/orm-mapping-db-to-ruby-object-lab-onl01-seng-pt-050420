28e97903b11430959e0facde2d137ecc8e1acc12
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-02 12:31:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,13 @@ class Student
   def self.first_X_students_in_grade_10()
   end
   
-  def self.
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = "10" LIMIT 1;
+    SQL
+    DB[:conn].execute(sql)
+  end
   
   def save
     sql = <<-SQL
